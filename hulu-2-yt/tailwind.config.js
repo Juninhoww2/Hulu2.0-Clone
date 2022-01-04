087be019c00889008fec0b476311d3bcf471c9c9
juninhoww2 <alqadianokira@gmail.com>
2022-01-04 10:48:52 -0300

    update

--- hulu-2-yt/tailwind.config.js
+++ hulu-2-yt/tailwind.config.js
@@ -1,8 +1,12 @@
 module.exports = {
-  content: [],
-  purge: ["./pages/**/*,{js,ts,jsx,tsx}", "./components/**/*{js,ts,jsx,tsx}"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('tailwind-scrollbar-hide')
+  ],
 }
